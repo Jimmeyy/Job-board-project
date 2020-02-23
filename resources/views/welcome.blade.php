b8df0739062255d1141dd2cebd721662c8ae5d91
Damian Żmuda <d.zmuda94@gmail.com>
2020-02-23 19:03:38 +0100

    Grid scss changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/app.css">
         <title>Job board</title>
+         
+        <style>
+            .box {
+                display: block;
+                width: 100%;
+                height: 80px;
+                border: 5px solid orangered;
+            }
+        </style>
+
+
     </head>
     <body>
-        <h1>Lorem ipsum</h1>
+        <div class="container">
+            <div class="row center-lg">
+                <div class="col-xs-12 col-sm-9 col-md-6 col-lg-offset-3 col-lg-3">
+                    <div class="box"></div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
