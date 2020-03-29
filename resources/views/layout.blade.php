c58ca46e35e7e2d1d9068d3f7afac0f6c24b8f7c
Damian Zmuda <d.zmuda94@gmail.com>
2020-03-29 15:28:06 +0200

    layout template changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>JobBoard - @yield('title')</title>
 </head>
 <body>
