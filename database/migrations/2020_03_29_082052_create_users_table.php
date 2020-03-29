7f9654aadaa91250e7e603023876dcb2aff6cae6
Damian Zmuda <d.zmuda94@gmail.com>
2020-03-29 11:51:48 +0200

    Update users table migration

--- database/migrations/2020_03_29_082052_create_users_table.php
+++ database/migrations/2020_03_29_082052_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->boolean('activated');
             $table->integer('type');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
