61a7eeb03aafe87db1099f43bc2c04ecec7831c5
Damian Zmuda <damian.zmuda@beefee.co.uk>
2020-03-31 20:15:45 +0200

    Add candidates create candidate table migration

---
+++ database/migrations/2020_03_31_180949_create_candidates_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCandidatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('candidates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->string('name')->nullable();
+            $table->string('surname')->nullable();
+            $table->string('city')->nullable();
+            $table->string('position')->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('about')->nullable();
+            $table->string('cv_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('candidates');
+    }
+}
