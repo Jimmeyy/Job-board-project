edcbc5c44bee3395a16d384db1b472faa4009ec2
Damian Zmuda <damian.zmuda@beefee.co.uk>
2020-03-31 20:19:27 +0200

    Add candidates create employers table migration

---
+++ database/migrations/2020_03_31_181619_create_employers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigIncrements('user_id');
+            $table->integer('owned_jobs')->nullable;
+            $table->string('name')->nullable;
+            $table->string('city')->nullable;
+            $table->date('start_date')->nullable;
+            $table->integer('team_size')->nullable;
+            $table->string('about')->nullable;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employers');
+    }
+}
