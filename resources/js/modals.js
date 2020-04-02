65f771ebdd4e17868dcc1ff4d7498c49d91028f3
Damian Zmuda <d.zmuda94@gmail.com>
2020-04-02 11:40:29 +0200

    update modals js logic

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,22 +1,26 @@
 const modals = function() {
     const activeClass = 'is-active';
 
-    function createModal(modalClass, modalBtnClass) {
+    function createModal(modalClass, modalBtnClass, modalQueryParam) {
         const wrapper = document.querySelector(modalClass);
         const main = wrapper.querySelector('.modal-main');
         const openBtn = document.querySelector(modalBtnClass);
         const closeBtn = wrapper.querySelector('.modal-close-js');
+        const queryParam = modalQueryParam;
 
         const modal = {
             wrapper,
             main,
             openBtn,
             closeBtn,
+            queryParam,
         };
 
         modal.openBtn.addEventListener('click', () => open(modal));
         modal.closeBtn.addEventListener('click', () => close(modal));
         modal.wrapper.addEventListener('click', () => clickOutside(event, modal));
+
+        checkQueryParam(modal);
     }
 
     function open(modal) {
@@ -34,8 +38,18 @@ const modals = function() {
         }
     }
 
-    const modalLogin = createModal('.modal-login-js', '.modal-login-btn-js');
-    const modalRegister = createModal('.modal-register-js', '.modal-register-btn-js');
+    function checkQueryParam(modal) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const param = urlParams.get(modal.queryParam);
+        console.log(param);
+
+        if (param) {
+            open(modal);
+        }
+    }
+
+    const modalLogin = createModal('.modal-login-js', '.modal-login-btn-js', 'modalLogin');
+    const modalRegister = createModal('.modal-register-js', '.modal-register-btn-js', 'modalRegister');
 };
 
 export default modals;
