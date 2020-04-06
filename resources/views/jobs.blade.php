cc9e79215c2b259f88fcbb353fa692ba7b1e7f7c
Damian Zmuda <d.zmuda94@gmail.com>
2020-04-06 14:45:00 +0200

    Add styles for jobs-list view

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -30,116 +30,114 @@
     {{-- jobs list section --}}
     <section class="jobs-list-section">
         <div class="container">
-            <div class="jobs-list-header">
-                <h3>98 Jobs & Vacancies</h3>
-                <div class="jobs-filter-panel">
-                    <span>Sort by</span>
-                    <div class="input-wrapper margin-vertical">
-                        <div class="input dropdown border-grey icon-dropdown dropdown-js">
-                            <input type="hidden" value="">
-                            <span>Specialism</span>
-                            <ul>
-                                <li data-value="Doctor">Doctor</li>
-                                <li data-value="It">IT</li>
-                                <li data-value="Design">Design</li>
-                                <li data-value="Freelance">Freelance</li>
-                                <li data-value="Ziomo">Ziomo</li>
-                            </ul>
+            <div class="jobs-list-wrapper">
+                <div class="jobs-list-header">
+                    <h3>98 Jobs & Vacancies</h3>
+                    <div class="jobs-filter-panel">
+                        <span>Sort by</span>
+                        <div class="input-wrapper margin-horizontal">
+                            <div class="input dropdown border-grey icon-dropdown dropdown-js">
+                                <input type="hidden" value="">
+                                <span>Most recent</span>
+                                <ul>
+                                    <li data-value="most recent">Most recent</li>
+                                    <li data-value="most popular">Most popular</li>
+                                    <li data-value="most viewed">Most viewed</li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                    <div class="input-wrapper margin-vertical">
-                        <div class="input dropdown border-grey icon-dropdown dropdown-js">
-                            <input type="hidden" value="">
-                            <span>Specialism</span>
-                            <ul>
-                                <li data-value="Doctor">Doctor</li>
-                                <li data-value="It">IT</li>
-                                <li data-value="Design">Design</li>
-                                <li data-value="Freelance">Freelance</li>
-                                <li data-value="Ziomo">Ziomo</li>
-                            </ul>
+                        <div class="input-wrapper margin-horizontal">
+                            <div class="input dropdown border-grey icon-dropdown dropdown-js">
+                                <input type="hidden" value="">
+                                <span>12</span>
+                                <ul>
+                                    <li data-value="12">12</li>
+                                    <li data-value="24">24</li>
+                                    <li data-value="36">36</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="jobs-list">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="job-box">
-                            <div class="job-box-header">
-                                <div class="badge purple">Full time</div>
-                                <div class="icon-box favorite margin-right"></div>
-                            </div>
-                            <div class="job-box-content">
-                                <h3>Web designer/Developer</h3>
-                                <h4>Massimo Artemisis</h4>
-                                <div class="job-box-footer">
-                                    <span>Sacramento, California</span>
-                                    <a href="#" class="job-box-btn">Apply now</a>
+                <div class="jobs-list">
+                    <div class="row">
+                        <div class="col-xs-12 col-lg-4">
+                            <div class="job-box">
+                                <div class="job-box-header">
+                                    <div class="badge pink">Full time</div>
+                                    <div class="icon-box favorite margin-right"></div>
+                                </div>
+                                <div class="job-box-content">
+                                    <h3>Web designer/Developer</h3>
+                                    <h4>Massimo Artemisis</h4>
+                                    <div class="job-box-footer">
+                                        <span>Sacramento, California</span>
+                                        <a href="#" class="job-box-btn">Apply now</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="job-box">
-                            <div class="job-box-header">
-                                <div class="badge purple">Full time</div>
-                                <div class="icon-box favorite margin-right"></div>
-                            </div>
-                            <div class="job-box-content">
-                                <h3>Web designer/Developer</h3>
-                                <h4>Massimo Artemisis</h4>
-                                <div class="job-box-footer">
-                                    <span>Sacramento, California</span>
-                                    <a href="#" class="job-box-btn">Apply now</a>
+                        <div class="col-xs-12 col-lg-4">
+                            <div class="job-box">
+                                <div class="job-box-header">
+                                    <div class="badge pink">Full time</div>
+                                    <div class="icon-box favorite margin-right"></div>
+                                </div>
+                                <div class="job-box-content">
+                                    <h3>Web designer/Developer</h3>
+                                    <h4>Massimo Artemisis</h4>
+                                    <div class="job-box-footer">
+                                        <span>Sacramento, California</span>
+                                        <a href="#" class="job-box-btn">Apply now</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="job-box">
-                            <div class="job-box-header">
-                                <div class="badge purple">Full time</div>
-                                <div class="icon-box favorite margin-right"></div>
-                            </div>
-                            <div class="job-box-content">
-                                <h3>Web designer/Developer</h3>
-                                <h4>Massimo Artemisis</h4>
-                                <div class="job-box-footer">
-                                    <span>Sacramento, California</span>
-                                    <a href="#" class="job-box-btn">Apply now</a>
+                        <div class="col-xs-12 col-lg-4">
+                            <div class="job-box">
+                                <div class="job-box-header">
+                                    <div class="badge pink">Full time</div>
+                                    <div class="icon-box favorite margin-right"></div>
+                                </div>
+                                <div class="job-box-content">
+                                    <h3>Web designer/Developer</h3>
+                                    <h4>Massimo Artemisis</h4>
+                                    <div class="job-box-footer">
+                                        <span>Sacramento, California</span>
+                                        <a href="#" class="job-box-btn">Apply now</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="job-box">
-                            <div class="job-box-header">
-                                <div class="badge purple">Full time</div>
-                                <div class="icon-box favorite margin-right"></div>
-                            </div>
-                            <div class="job-box-content">
-                                <h3>Web designer/Developer</h3>
-                                <h4>Massimo Artemisis</h4>
-                                <div class="job-box-footer">
-                                    <span>Sacramento, California</span>
-                                    <a href="#" class="job-box-btn">Apply now</a>
+                        <div class="col-xs-12 col-lg-4">
+                            <div class="job-box">
+                                <div class="job-box-header">
+                                    <div class="badge pink">Full time</div>
+                                    <div class="icon-box favorite margin-right"></div>
+                                </div>
+                                <div class="job-box-content">
+                                    <h3>Web designer/Developer</h3>
+                                    <h4>Massimo Artemisis</h4>
+                                    <div class="job-box-footer">
+                                        <span>Sacramento, California</span>
+                                        <a href="#" class="job-box-btn">Apply now</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="job-box">
-                            <div class="job-box-header">
-                                <div class="badge purple">Full time</div>
-                                <div class="icon-box favorite margin-right"></div>
-                            </div>
-                            <div class="job-box-content">
-                                <h3>Web designer/Developer</h3>
-                                <h4>Massimo Artemisis</h4>
-                                <div class="job-box-footer">
-                                    <span>Sacramento, California</span>
-                                    <a href="#" class="job-box-btn">Apply now</a>
+                        <div class="col-xs-12 col-lg-4">
+                            <div class="job-box">
+                                <div class="job-box-header">
+                                    <div class="badge pink">Full time</div>
+                                    <div class="icon-box favorite margin-right"></div>
+                                </div>
+                                <div class="job-box-content">
+                                    <h3>Web designer/Developer</h3>
+                                    <h4>Massimo Artemisis</h4>
+                                    <div class="job-box-footer">
+                                        <span>Sacramento, California</span>
+                                        <a href="#" class="job-box-btn">Apply now</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -154,7 +152,7 @@
                                 <span>Prev</span>
                             </a>
                         </li>
-                        <li>
+                        <li class="is-active">
                             <a href="#">1</a>
                         </li>
                         <li>
